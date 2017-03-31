01f768a24ea8efcb7caabf7ee85540b4ad8267a7
mrSerega <sega00796@mail.ru>
2017-03-31 16:00:51 +0300

    +good_kruskal

---
+++ kruskal/kruskal/good_kruskal.py
@@ -0,0 +1,42 @@
+
+
+def good_kruskal(arcs, number_of_nodes, parents = [], result = []):
+	
+	cost = 0
+
+	numbers = []
+
+	arcs.sort()
+	
+	for i in xrange(number_of_nodes):
+		parents.append(-1)
+		result.append(-1)
+		numbers.append(i)
+
+
+	for arc in arcs:
+		first_parent = find(arc[1], parents)
+		second_parent = find(arc[2], parents)
+
+		#print 'parent of',arc[1],'is', first_parent
+		#print 'parent of', arc[2], 'is', second_parent
+		
+		if first_parent != second_parent:
+			union(first_parent,arc[2], parents)
+			union(arc[1],arc[2],result)
+
+	#print numbers
+	print parents
+	print result
+
+def find(node, parents):
+	while(parents[node] != -1):
+		node = parents[node]
+	return node
+
+def union(first, second, parents):
+	parents[second] = first
+
+
+if __name__ == '__main__':
+	good_kruskal()
---
+++ kruskal/kruskal/main.py
@@ -0,0 +1,13 @@
+﻿import good_kruskal as gk
+import bad_kruskal as bk
+
+def main():
+	n = 7 #input here
+	arcs = [(3,0,1),(1,0,2),(2,0,4),(7,0,5),(9,1,2),(2,2,3),(8,2,4),(4,3,4),(5,3,6),(5,4,5),(7,4,6),(4,5,6)] #input here
+	parents = []
+	res = gk.good_kruskal(arcs,7, parents)
+
+	bk.bad_kruskal(arcs, 7)
+
+if __name__ == '__main__':
+	main()
